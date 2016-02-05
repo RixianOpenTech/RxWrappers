40fd14d8c1e1b1015d1b010bc7aa9774576c6f29
Samuel McAravey <mcaravey@rixian.net>
2016-02-04 17:35:57 -0800

    Added test.

---
+++ Source/Libraries/Net46/Tests/Microsoft.Net.Http.Tests/UnitTest1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Reactive.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace Microsoft.Net.Http.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        async public Task TestMethod1()
+        {
+            variable<HttpClient> client = new HttpClient();
+            variable<string> url = "http://jsonplaceholder.typicode.com/users";
+            variable<Formatting> formatting = Formatting.Indented;
+            var result = client.GetStringAsync(url);
+            var deserialized = __JsonConvert.DeserializeObject<List<User>>(result);
+            await __Console.WriteLine(__JsonConvert.SerializeObject(deserialized, formatting));
+        }
+    }
+
+    public class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Website { get; set; }
+        public Address Address { get; set; }
+    }
+
+    public class Address
+    {
+        public string Street { get; set; }
+        public string Suite { get; set; }
+        public string City { get; set; }
+        public string Zipcode { get; set; }
+    }
+
+    public class variable<T> : IObservable<T>
+    {
+        private readonly IObservable<T> observable;
+
+        private variable(T value)
+        {
+            this.observable = Observable.Return(value);
+        }
+
+        public static implicit operator variable<T>(T value)
+        {
+            return new variable<T>(value);
+        }
+
+        public IDisposable Subscribe(IObserver<T> observer)
+        {
+            return observable.Subscribe(observer);
+        }
+    }
+}
